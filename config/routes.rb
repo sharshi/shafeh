a57413194e4105c90a22ed5b326ead276b1a53ca
sharshi <sharshib@gmail.com>
2020-01-03 10:03:06 -0500

    added db

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  direct :homepage do
+    "application"
+  end
 end
